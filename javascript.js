a27e3103807f950765ae90c5685aa7fbc64641f5
marco <migliavacca.marco08@gmail.com>
2022-05-11 08:46:46 +0200

    Add computerPlayGenerator and computerPlay functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Test")
+function computerPlayGenerator() {
+   return Math.floor(Math.random() * 3 + 0)
+}
+
+function computerPlay() {
+   let num = computerPlayGenerator()
+   if (num == 0) {
+      return "Rock"
+   } else if (num == 1){
+      return "Paper"
+   } else {
+      return ("Scissors")
+   }
+}
+
+console.log(computerPlay())
